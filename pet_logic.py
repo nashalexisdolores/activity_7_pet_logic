308b9a78cd375d048f9f312491128cd78644ea68
nashalexisdolores <alxsdlrs@gmail.com>
2026-06-01 21:32:41 +0800

    implement set_name and set_animal_type; add controlled data mutators

--- pet_logic.py
+++ pet_logic.py
@@ -3,3 +3,9 @@ class pet_logic:
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
