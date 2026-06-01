ca62d5c5fdd29b642c65eeedb980a7b121f1248d
nashalexisdolores <alxsdlrs@gmail.com>
2026-06-01 21:34:22 +0800

    add set_age and accessor methods; complete read/write interface for pet data

--- pet_logic.py
+++ pet_logic.py
@@ -9,3 +9,15 @@ class pet_logic:
 
     def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
