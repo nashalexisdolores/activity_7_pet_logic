6fd3bcd36937e21efeb28a314f5d5330b95c66c3
nashalexisdolores <alxsdlrs@gmail.com>
2026-06-01 21:35:57 +0800

    setup pet_app class; initialize tkinter root for pet registry UI

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from pet_logic import pet_logic
+
+class pet_app:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Pet Registry v1.0")
+        self.root.geometry("400x500")
+
+        
